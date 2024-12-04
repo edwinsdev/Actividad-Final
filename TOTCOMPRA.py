09147c21992bbff136be53169ac3f920d289b951
edwinsdev <edwinbestdev@hotmail.com>
2024-12-04 18:35:55 -0500

    Commit Final

---
+++ TOTCOMPRA.py
@@ -0,0 +1,8 @@
+gasto = float(input("Ingresa el total de tus compras"))
+recompensa = 0
+if gasto > 100:
+    recompensa = gasto * 0.10
+elif 50 <= gasto <= 100:
+    recompensa = gasto * 0.05
+
+print(f"Tu recompensa es de ${recompensa:.2f}")
